880dddc133349d97aca6e304471a298bc6294b4a
Pappu Lal <73828213+PappuLal1234@users.noreply.github.com>
2021-04-16 12:29:17 +0530

    Add files via upload

---
+++ footer.inc.php
@@ -0,0 +1,31 @@
+<div class="clearfix"></div>
+         <footer class="site-footer">
+            <div class="footer-inner bg-white">
+               <div class="row">
+                  <div class="col-sm-6">
+                     Copyright &copy; 2021 PappuPajer
+                  </div>
+                  <div class="col-sm-6 text-right">
+                     Designed by <a href="#">Partusha</a>
+                  </div>
+               </div>
+            </div>
+         </footer>
+      </div>
+      <script src="assets/js/vendor/jquery-2.1.4.min.js" type="text/javascript"></script>
+      <script src="assets/js/popper.min.js" type="text/javascript"></script>
+      <script src="assets/js/plugins.js" type="text/javascript"></script>
+      <script src="assets/js/main.js" type="text/javascript"></script>
+   </body>
+</html>
+ <div style="clear:both" class="panel panel-default">
+                 <div class="col-sm-6">
+                     Copyright &copy; 2021 PappuPajer
+                  </div>
+    
+    
+    <div class="panel-body">
+      
+    </div>
+    <div class="panel-footer">Panel footer</div>
+  </div> 
